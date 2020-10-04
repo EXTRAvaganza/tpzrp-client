79afab5d25b2c5f346c3cad8ab56138280673085
EXTRAvaganza <rafael.manzhukov@yandex.ru>
2020-10-04 19:41:12 +0400

    [*]idk

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import Client.Client;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        try {
+            new Client("localhost",4444).close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
